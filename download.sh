e03b872881d767009d24cbba26a389f61145c1c5
RehabMan <racerrehabman@gmail.com>
2016-07-30 18:58:40 -0700

    adjust to changes at bitbucket download HTML

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ function download()
 {
     echo "downloading $2:"
     curl --location --silent --output /tmp/org.rehabman.download.txt https://bitbucket.org/RehabMan/$1/downloads
-    scrape=`grep -o -m 1 href\=\".*$2.*\.zip.*\" /tmp/org.rehabman.download.txt|perl -ne 'print $1 if /href\=\"(.*)\"/'`
+    scrape=`grep -o -m 1 "/RehabMan/$1/downloads/$2.*\.zip" /tmp/org.rehabman.download.txt|perl -ne 'print $1 if /(.*)\"/'`
     url=https://bitbucket.org$scrape
     echo $url
     if [ "$3" == "" ]; then
