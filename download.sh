030ad4fc593b5993f5d325d5124d3754cf5ab557
RehabMan <racerrehabman@gmail.com>
2018-09-23 15:14:31 -0700

    refactor to use hack-tools project

--- download.sh
+++ download.sh
@@ -1,77 +1,51 @@
+#!/bin/bash
 #set -x
-curl_options="--retry 5 --location --progress-bar"
-curl_options_silent="--retry 5 --location --silent"
 
-# download typical release from RehabMan bitbucket downloads
-function download()
-# $1 is subdir on rehabman bitbucket
-# $2 is prefix of zip file name
-{
-    echo "downloading $2:"
-    curl $curl_options_silent --output /tmp/org.rehabman.download.txt https://bitbucket.org/RehabMan/$1/downloads/
-    local scrape=`grep -o -m 1 "/RehabMan/$1/downloads/$2.*\.zip" /tmp/org.rehabman.download.txt|perl -ne 'print $1 if /(.*)\"/'`
-    local url=https://bitbucket.org$scrape
-    echo $url
-    if [ "$3" == "" ]; then
-        curl $curl_options --remote-name "$url"
-    else
-        curl $curl_options --output "$3" "$url"
-    fi
-    echo
-}
+# get copy of tools if not present
+if [[ ! -d ./tools ]]; then
+    git clone https://github.com/RehabMan/hack-tools.git tools
+fi
+# update tools to latest
+if [[ -e ./tools/.git ]]; then
+    cd ./tools && git pull
+    cd ..
+fi
 
-# download latest release from github (perhaps others)
-function download_latest_notbitbucket()
-# $1 is main URL
-# $2 is URL of release page
-# $3 is partial file name to look for
-# $4 is file name to rename to
-{
-    echo "downloading latest $4 from $2:"
-    curl $curl_options_silent --output /tmp/org.rehabman.download.txt "$2"
-    local scrape=`grep -o -m 1 "/.*$3.*\.zip" /tmp/org.rehabman.download.txt`
-    local url=$1$scrape
-    echo $url
-    curl $curl_options --output "$4" "$url"
-    echo
-}
+# include subroutines
+DIR=$(dirname ${BASH_SOURCE[0]})
+source "$DIR/tools/_download_subs.sh"
 
-if [ ! -d ./downloads ]; then mkdir ./downloads; fi && rm -Rf downloads/* && cd ./downloads
+# remove deprecated downloads directory to avoid confusion
+if [[ -e ./downloads ]]; then rm -Rf ./downloads; fi
+
+# create _downloads directory and clean
+if [[ ! -d ./_downloads ]]; then mkdir ./_downloads; fi && rm -Rf ./_downloads/* && cd ./_downloads
 
 # download kexts
 mkdir ./kexts && cd ./kexts
-download os-x-fakesmc-kozlek RehabMan-FakeSMC
-download os-x-voodoo-ps2-controller RehabMan-Voodoo
-download os-x-realtek-network RehabMan-Realtek-Network
-download os-x-intel-network RehabMan-IntelMausiEthernet
+download_rehabman os-x-fakesmc-kozlek RehabMan-FakeSMC
+download_rehabman os-x-voodoo-ps2-controller RehabMan-Voodoo
+download_rehabman os-x-realtek-network RehabMan-Realtek-Network
 #download os-x-acpi-backlight RehabMan-Backlight
 #download os-x-intel-backlight RehabMan-IntelBacklight
-download os-x-acpi-battery-driver RehabMan-Battery
-download os-x-eapd-codec-commander RehabMan-CodecCommander
-download os-x-fake-pci-id RehabMan-FakePCIID
-download os-x-brcmpatchram RehabMan-BrcmPatchRAM
-download os-x-atheros-3k-firmware RehabMan-Atheros
-#download os-x-acpi-poller RehabMan-Poller
-download os-x-usb-inject-all RehabMan-USBInjectAll
+download_rehabman os-x-acpi-battery-driver RehabMan-Battery
+download_rehabman os-x-eapd-codec-commander RehabMan-CodecCommander
+download_rehabman os-x-fake-pci-id RehabMan-FakePCIID
+download_rehabman os-x-brcmpatchram RehabMan-BrcmPatchRAM
+download_rehabman os-x-atheros-3k-firmware RehabMan-Atheros
+download_rehabman os-x-usb-inject-all RehabMan-USBInjectAll
 #download os-x-acpi-debug RehabMan-Debug
-download_latest_notbitbucket "https://github.com" "https://github.com/vit9696/Lilu/releases" "RELEASE" "nbb_vit9696-Lilu.zip"
-#download_latest_notbitbucket "https://github.com" "https://github.com/lvs1974/IntelGraphicsFixup/releases" "RELEASE" "nbb_lvs1974-IntelGraphicsFixup.zip"
-#download_latest_notbitbucket "https://github.com" "https://github.com/PMHeart/CoreDisplayFixup/releases" "RELEASE" "nbb_PMHeart-CoreDisplayFixup.zip"
-#download_latest_notbitbucket "https://github.com" "https://github.com/BarbaraPalvin/IntelGraphicsDVMTFixup/releases" "IntelGraphicsDVMTFixup" "nbb_BarbaraPalvin-IntelGraphicsDVMTFixup.zip"
-# WhateverGreen.kext replaces most of the Lilu graphics plugins
-download_latest_notbitbucket "https://github.com" "https://github.com/acidanthera/WhateverGreen/releases" "RELEASE" "nbb_acidanthera-WhateverGreen.zip"
-download_latest_notbitbucket "https://github.com" "https://github.com/acidanthera/AirportBrcmFixup/releases" "RELEASE" nbb_acidanthera-AirportBrcmFixup.zip
-download_latest_notbitbucket "https://github.com" "https://github.com/acidanthera/BT4LEContiunityFixup/releases" "RELEASE" nbb_acidanthera-BT4LEContiunityFixup.zip
+download_acidanthera Lilu acidanthera-Lilu
+download_acidanthera WhateverGreen acidanthera-WhateverGreen
+download_acidanthera AirportBrcmFixup acidanthera-AirportBrcmFixup
+download_acidanthera BT4LEContiunityFixup acidanthera-BT4LEContiunityFixup
 cd ..
 
 # download tools
 mkdir ./tools && cd ./tools
-download os-x-maciasl-patchmatic RehabMan-patchmatic
-download os-x-maciasl-patchmatic RehabMan-MaciASL
-download acpica iasl iasl.zip
+download_rehabman os-x-maciasl-patchmatic RehabMan-patchmatic
+download_rehabman os-x-maciasl-patchmatic RehabMan-MaciASL
+download_rehabman acpica iasl iasl.zip
 cd ..
 
-# download Clover related (HPFanReset.efi, as example only, only for ProBook, so disabled)
-mkdir ./efi && cd ./efi
-#download hp-probook-4x30s-fan-reset HPFanReset
-cd ..
+#EOF
--- install_downloads.sh
+++ install_downloads.sh
@@ -1,213 +1,47 @@
+#!/bin/bash
 #set -x
 
-SUDO=sudo
-#SUDO='echo #'
-#SUDO=nothing
-TAG=tag_file
-TAGCMD=`pwd`/tools/tag
-SLE=/System/Library/Extensions
-LE=/Library/Extensions
-EXCEPTIONS="Sensors|FakePCIID_AR9280|FakePCIID_Intel_HD_Graphics|FakePCIID_Broadcom_WiFi.kext|BrcmPatchRAM|BrcmBluetoothInjector|BrcmFirmwareData|USBInjectAll|WhateverName"
-ESSENTIAL="FakeSMC.kext RealtekRTL8111.kext IntelMausiEthernet.kext USBInjectAll.kext Lilu.kext WhateverGreen.kext AppleBacklightInjector.kext IntelBacklight.kext"
+# customize EXCEPTIONS and ESSENTIAL as needed
+EXCEPTIONS=
+ESSENTIAL=
+# customize HDA to indicate your HDA resources/HDA codec
+#HDA=ALC283
 
-# extract minor version (eg. 10.9 vs. 10.10 vs. 10.11)
-MINOR_VER=$([[ "$(sw_vers -productVersion)" =~ [0-9]+\.([0-9]+) ]] && echo ${BASH_REMATCH[1]})
+# include subroutines
+DIR=$(dirname ${BASH_SOURCE[0]})
+source "$DIR/tools/_install_subs.sh"
 
-# install to /Library/Extensions for 10.11 or greater
-if [[ $MINOR_VER -ge 11 ]]; then
-    KEXTDEST=$LE
-else
-    KEXTDEST=$SLE
-fi
+warn_about_superuser
 
-# this could be removed if 'tag' can be made to work on old systems
-function tag_file
-{
-    if [[ $MINOR_VER -ge 9 ]]; then
-        $SUDO "$TAGCMD" "$@"
-    fi
-}
+# install tools
+install_tools
 
-function check_directory
-{
-    for x in $1; do
-        if [ -e "$x" ]; then
-            return 1
-        else
-            return 0
-        fi
-    done
-}
+# install required kexts
+install_download_kexts
+install_brcmpatchram_kexts
+remove_deprecated_kexts
+install_backlight_kexts
 
-function nothing
-{
-    :
-}
+# install special download kexts
+install_kext _downloads/kexts/RehabMan-FakePCIID*/Release/FakePCIID_AR9280_as_AR946x.kext
+install_kext _downloads/kexts/RehabMan-FakePCIID*/Release/FakePCIID_XHCIMux.kext
 
-function remove_kext
-{
-    $SUDO rm -Rf $SLE/"$1" $LE/"$1"
-}
+# EHCI is disabled, so no need for FakePCIID_XHCIMux.kext
+#remove_kext FakePCIID_XHCIMux.kext
 
-function install_kext
-{
-    if [ "$1" != "" ]; then
-        echo installing $1 to $KEXTDEST
-        remove_kext `basename $1`
-        $SUDO cp -Rf $1 $KEXTDEST
-        $TAG -a Gray $KEXTDEST/`basename $1`
-    fi
-}
+# create/install patched AppleHDA files
+install_hda
 
-function install_app
-{
-    if [ "$1" != "" ]; then
-        echo installing $1 to /Applications
-        $SUDO rm -Rf /Applications/`basename $1`
-        $SUDO cp -Rf $1 /Applications
-        $TAG -a Gray /Applications/`basename $1`
-    fi
-}
+# remove other kexts not used anymore...
+#remove_kext Example.kext
 
-function install_binary
-{
-    if [ "$1" != "" ]; then
-        echo installing $1 to /usr/bin
-        $SUDO rm -f /usr/bin/`basename $1`
-        $SUDO cp -f $1 /usr/bin
-        $TAG -a Gray /usr/bin/`basename $1`
-    fi
-}
+# all kexts are now installed, so rebuild cache
+rebuild_kernel_cache
 
-function install
-{
-    installed=0
-    out=${1/.zip/}
-    rm -Rf $out/* && unzip -q -d $out $1
-    check_directory $out/Release/*.kext
-    if [ $? -ne 0 ]; then
-        for kext in $out/Release/*.kext; do
-            # install the kext when it exists regardless of filter
-            kextname="`basename $kext`"
-            if [[ -e "$SLE/$kextname" || -e "$KEXTDEST/$kextname" || "$2" == "" || "`echo $kextname | grep -vE "$2"`" != "" ]]; then
-                install_kext $kext
-            fi
-        done
-        installed=1
-    fi
-    check_directory $out/*.kext
-    if [ $? -ne 0 ]; then
-        for kext in $out/*.kext; do
-            # install the kext when it exists regardless of filter
-            kextname="`basename $kext`"
-            if [[ -e "$SLE/$kextname" || -e "$KEXTDEST/$kextname" || "$2" == "" || "`echo $kextname | grep -vE "$2"`" != "" ]]; then
-                install_kext $kext
-            fi
-        done
-        installed=1
-    fi
-    check_directory $out/Release/*.app
-    if [ $? -ne 0 ]; then
-        for app in $out/Release/*.app; do
-            # install the app when it exists regardless of filter
-            appname="`basename $app`"
-            if [[ -e "/Applications/$appname" || -e "/Applications/$appname" || "$2" == "" || "`echo $appname | grep -vE "$2"`" != "" ]]; then
-                install_app $app
-            fi
-        done
-        installed=1
-    fi
-    check_directory $out/*.app
-    if [ $? -ne 0 ]; then
-        for app in $out/*.app; do
-            # install the app when it exists regardless of filter
-            appname="`basename $app`"
-            if [[ -e "/Applications/$appname" || -e "/Applications/$appname" || "$2" == "" || "`echo $appname | grep -vE "$2"`" != "" ]]; then
-                install_app $app
-            fi
-        done
-        installed=1
-    fi
-    if [ $installed -eq 0 ]; then
-        check_directory $out/*
-        if [ $? -ne 0 ]; then
-            for tool in $out/*; do
-                install_binary $tool
-            done
-        fi
-    fi
-}
+# update kexts on EFI/CLOVER/kexts/Other
+update_efi_kexts
 
-if [ "$(id -u)" != "0" ]; then
-    echo "This script requires superuser access..."
-fi
+# VoodooPS2Daemon is deprecated
+remove_voodoops2daemon
 
-# unzip/install kexts
-check_directory ./downloads/kexts/*.zip
-if [ $? -ne 0 ]; then
-    echo Installing kexts...
-    cd ./downloads/kexts
-    for kext in *.zip; do
-        install $kext "$EXCEPTIONS"
-    done
-    if [[ $MINOR_VER -ge 11 ]]; then
-        # 10.11 needs BrcmPatchRAM2.kext
-        cd RehabMan-BrcmPatchRAM*/Release && install_kext BrcmPatchRAM2.kext && cd ../..
-        # 10.11 needs USBInjectAll.kext
-        cd RehabMan-USBInjectAll*/Release && install_kext USBInjectAll.kext && cd ../..
-        # remove BrcPatchRAM.kext just in case
-        remove_kext BrcmPatchRAM.kext
-        # remove injector just in case
-        remove_kext BrcmBluetoothInjector.kext
-    else
-        # prior to 10.11, need BrcmPatchRAM.kext
-        cd RehabMan-BrcmPatchRAM*/Release && install_kext BrcmPatchRAM.kext && cd ../..
-        # remove BrcPatchRAM2.kext just in case
-        remove_kext BrcmPatchRAM2.kext
-        # remove injector just in case
-        remove_kext BrcmBluetoothInjector.kext
-    fi
-    # this guide does not use BrcmFirmwareData.kext
-    remove_kext BrcmFirmwareData.kext
-    # now using IntelBacklight.kext instead of ACPIBacklight.kext
-    remove_kext ACPIBacklight.kext
-    # deal with some renames
-    remove_kext FakePCIID_BCM94352Z_as_BCM94360CS2.kext
-    remove_kext FakePCIID_HD4600_HD4400.kext
-    # IntelGraphicsFixup.kext is no longer used (replaced by WhateverGreen.kext)
-    remove_kext IntelGraphicsFixup.kext
-    # FakePCIID_Intel_HD_Graphics.kext not needed either
-    remove_kext FakePCIID_Intel_HD_Graphics.kext
-    # using AirportBrcmFixup.kext instead of FakePCIID_Broadcom_WiFi.kext
-    remove_kext FakePCIID_Broadcom_WiFi.kext
-    cd ../..
-fi
-
-# force cache rebuild with output
-$SUDO touch $SLE && $SUDO kextcache -u /
-
-# install/update kexts on EFI/Clover/kexts/Other
-EFI=`./mount_efi.sh`
-echo Updating kexts at EFI/Clover/kexts/Other
-for kext in $ESSENTIAL; do
-    if [[ -e $KEXTDEST/$kext ]]; then
-        echo updating $EFI/EFI/CLOVER/kexts/Other/$kext
-        cp -Rf $KEXTDEST/$kext $EFI/EFI/CLOVER/kexts/Other
-    fi
-    rm -Rf $EFI/EFI/CLOVER/kexts/Other/IntelGraphicsFixup.kext
-done
-
-# unzip/install tools
-check_directory ./downloads/tools/*.zip
-if [ $? -ne 0 ]; then
-    echo Installing tools...
-    cd ./downloads/tools
-    for tool in *.zip; do
-        install $tool
-    done
-    cd ../..
-fi
-
-# remove VoodooPS2Daemon (deprecated)
-$SUDO rm -f /usr/bin/VoodooPS2Daemon
-$SUDO rm -f /Library/LaunchDaemons/org.rehabman.voodoo.driver.Daemon.plist
+#EOF
