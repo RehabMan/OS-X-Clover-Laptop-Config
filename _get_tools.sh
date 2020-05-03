a447cb2070ec6cc4e38bcaca09d2746a49a51dff
Bryce Glover <RandomDSdevel@gmail.com>
2020-05-03 15:22:37 -0400

    _get_tools.sh:  Force adding `_tools` when creating it as a submodule.
         This is required because `_tools` is explicitly listed in the repository's
    `.gitignore` file.

--- _get_tools.sh
+++ _get_tools.sh
@@ -3,7 +3,7 @@
 
 # get copy of tools if not present
 if [[ ! -d ./_tools && -z "$(git -C ./_tools/ rev-parse --show-superproject-working-tree)" ]]; then
-    git submodule add --name hack-tools https://github.com/RehabMan/hack-tools.git ./_tools
+    git submodule add -f --name hack-tools https://github.com/RehabMan/hack-tools.git ./_tools
     git submodule update --init ./_tools
 fi
 # update tools to latest
